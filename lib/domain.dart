63a1025d7023ba8f2dfd7ddd19d7b7524e177a62
gbougeard <gbougeard@interface-tech.com>
2014-02-27 18:52:47 +0100

    fix

--- lib/domain.dart
+++ lib/domain.dart
@@ -14,6 +14,33 @@ class Opinion {
   Opinion(this.id, this.hotelid, this.custid, this.note, this.welcome, this.atmosphere, this.comfort);
 
   factory Opinion.fromJsonMap(Map json) => new Opinion(json['id'], json['hotelid'], json['custid'], json['note'], json['welcome'], json['atmosphere'], json['comfort']);
-  Map toJson() => {'id': id, 'hotelid': hotelid, 'custid': custid, 'note': note, 'welcome': welcome, 'atmosphere': atmosphere, 'comfort': comfort};
+
+  Map toJson() => {
+      'id': id, 'hotelid': hotelid, 'custid': custid, 'note': note, 'welcome': welcome, 'atmosphere': atmosphere, 'comfort': comfort
+  };
+
+  String toJsonString() => JSON.encode(toJson());
+}
+
+class Hotel {
+  int id;
+  String name;
+  String city;
+  String state;
+  String countryCode;
+  String addr1;
+  String addr2;
+  String zipCode;
+  double latitude;
+  double longitude;
+
+  Hotel(this.id, this.name, this.city, this.state, this.countryCode, this.addr1, this.addr2, this.zipCode, this.latitude, this.longitude);
+
+  factory Hotel.fromJsonMap(Map json) => new Hotel(json['id'], json['name'], json['city'], json['state'], json['countryCode'], json['addr1'], json['addr2'], json['zipCode'], json['latitude'], json['longitude']);
+
+  Map toJson() => {
+      'id': id, 'name': name, 'city': city, 'state': state, 'countryCode': countryCode, 'addr1': addr1, 'addr2': addr2, 'zipCode': zipCode, 'latitude':latitude, 'longitude':longitude
+  };
+
   String toJsonString() => JSON.encode(toJson());
 }
